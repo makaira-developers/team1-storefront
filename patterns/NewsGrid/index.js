9fa78236445d90baa6d6d1c461b3c51453319621
p.koch <p.koch@metamorph.de>
2022-01-21 11:20:32 +0100

    Add NewsGrid Component

---
+++ patterns/NewsGrid/index.js
@@ -0,0 +1,6 @@
+function NewsGrid() {
+  return <section className="news-grid">NewsGrid</section>
+}
+
+export default NewsGrid
+export { default as newsGridVariants } from './variants.js'
---
+++ patterns/NewsGrid/variants.js
@@ -0,0 +1,35 @@
+const news = [
+  {
+    featuredImage: '',
+    title: '',
+    excerpt: '',
+    link: '',
+  },
+  {
+    featuredImage: '',
+    title: '',
+    excerpt: '',
+    link: '',
+  },
+  {
+    featuredImage: '',
+    title: '',
+    excerpt: '',
+    link: '',
+  },
+  {
+    featuredImage: '',
+    title: '',
+    excerpt: '',
+    link: '',
+  },
+]
+
+export default [
+  {
+    name: 'Default',
+    props: {
+      news,
+    },
+  },
+]
